caba01fcaaa605699126f9a1ce81ae7b711141dd
Juan <satelimportadores@gmail.com>
2020-08-06 11:17:20 -0500

    Creacion de componentes y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Route} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginInComponent } from './login-in/login-in.component';
+import { LoginOutComponent } from './login-out/login-out.component';
+
+const routes: Route[]=[
+  {path: '',component: LoginComponent},
+  {path: 'loginIn',component: LoginInComponent},
+  {path: 'loginOut',component: LoginOutComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginInComponent,
+    LoginOutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login-out/login-out.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-out',
+  templateUrl: './login-out.component.html',
+  styleUrls: ['./login-out.component.css']
+})
+export class LoginOutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
